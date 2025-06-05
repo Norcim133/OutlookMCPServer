ec86b63f0339f2eacf958d534df8ca7d8a9d5b95
Norcim133 <ian.hill@gmail.com>
2025-06-05 14:14:35 +1000

    Do not render shadows around chat input

--- ui/custom_styles.py
+++ ui/custom_styles.py
@@ -21,10 +21,12 @@ def alternate_chat_side_style():
     )
 
 def container_shadow_styles():
-    #Only adds shadow to stVerticalBlockBorderWrapper IN stVerticalBolock but NOT in aria-label="dialog"
     shadow_css = """
     <style>
-    [data-testid="stVerticalBlock"] > [data-testid="stVerticalBlockBorderWrapper"]:not([aria-label="dialog"] *)  {
+    /* Apply shadow to stVerticalBlockBorderWrapper when it's a direct child of stVerticalBlock,
+       BUT NOT if it's part of a dialog,
+       AND NOT if it contains an stChatInput element anywhere inside it. */
+    [data-testid="stVerticalBlock"] > [data-testid="stVerticalBlockBorderWrapper"]:not([aria-label="dialog"] *):not(:has([data-testid="stChatInput"])) {
         box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
         border-radius: 0.5rem;
         transition: box-shadow 0.3s ease;
